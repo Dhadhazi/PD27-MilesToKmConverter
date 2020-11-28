a7091ef6f98ff3b904ac39417f87cdd8640da3a0
Dhadhazi <dhadhazi@gmail.com>
2020-11-28 19:08:53 +0100

    Ordering modification

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+
+def calculate(miles):
+    km = round(miles*1.60934,2)
+    calculated_label.config(text=km)
+
+
 window = Tk()
 window.title("Mile to Km Converter")
 window.config(padx=50, pady=50)
@@ -16,17 +22,10 @@ km_label.grid(column=2, row=1)
 calculated_label = Label(text="0")
 calculated_label.grid(column=1, row=1)
 
-
-def calculate(miles):
-    km = round(miles*1.60934,2)
-    calculated_label.config(text=km)
-
-
 input_field = Entry(width=5)
 input_field.grid(column=1, row=0)
 
 calculate_button = Button(text="Calculate", command=lambda: calculate(float(input_field.get())))
 calculate_button.grid(column=1, row=3)
-
 
 window.mainloop()
